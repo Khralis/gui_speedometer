3710aa7723f4936d54d77b36862a3ecf75410c16
Khralis <50895160+Khralis@users.noreply.github.com>
2021-10-19 19:54:32 -0400

    Fixed auto colors not working

--- gui_speedometer/client.lua
+++ gui_speedometer/client.lua
@@ -14,7 +14,7 @@ Citizen.CreateThread(function()
         if playerCar and GetPedInVehicleSeat(playerCar, - 1) == playerPed then
           carSpeed = GetEntitySpeed(playerCar)
           hash = GetEntityModel(playerCar)
-          speed = GetVehicleModelEstimatedMaxSpeed(playerCar)
+          speed = GetVehicleModelEstimatedMaxSpeed(hash)
           ms = math.ceil(speed * 2.23693629)
           mph = math.ceil(carSpeed * 2.23693629)
           kmh = math.ceil(carSpeed * 3.6)
